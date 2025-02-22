d0ff7669fa2673d7b1965e87bb4cfc4710950522
kvnos84 <kevin.osdesign@gmail.com>
2025-02-22 13:24:18 +0100

    for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Pokemon Array-list
 let pokemonList = [];
 let pokemonList = [
     {
@@ -18,3 +19,20 @@ let pokemonList = [
 ];
 
 console.log(pokemonList);
+
+/*
+   This loop goes through each Pokémon in the pokemonList array.
+   It uses document.write() to display the Pokémon's name and height
+   in a paragraph on the webpage.
+*/
+for (let i = 0; i < pokemonList.length; i++) {
+    let name = pokemonList[i].name;
+    let height = pokemonList[i].height;
+
+    let text =
+        height > 1
+            ? `<span class="card__front--name">${name}</span> (height: ${height}) - Wow, that's big!`
+            : `<span class="card__front--name">${name}</span> (height: ${height})`;
+
+    document.write(`<div class="card__front">${text}</div>`);
+}
